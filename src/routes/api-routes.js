23920753d05d20a7ef90464b3ad8d39a5739bc87
rahulsai1999 <noneuser2183@gmail.com>
2020-04-22 21:12:01 +0530

    formatting 2

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -1,28 +1,27 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    indexContact,
-    viewContact,
-    updateContact,
-    deleteContact
-} from '../controllers/contactController';
+  indexContact,
+  viewContact,
+  updateContact,
+  deleteContact,
+} from "../controllers/contactController";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: "API is working",
-        message: "Welcome to MVC model"
-    });
+router.get("/", (req, res) => {
+  res.json({
+    status: "API is working",
+    message: "Welcome to MVC model",
+  });
 });
 
 //controllers routes
-router.route('/contacts')
-    .get(indexContact)
-router.route('/contacts/:contact_id')
-    .get(viewContact)
-    .patch(updateContact)
-    .put(updateContact)
-    .delete(deleteContact);
-
+router.route("/contacts").get(indexContact);
+router
+  .route("/contacts/:contact_id")
+  .get(viewContact)
+  .patch(updateContact)
+  .put(updateContact)
+  .delete(deleteContact);
 
 export default router;
--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,12 +1,9 @@
-import { Router } from 'express';
-import {
-    LoginContact,
-    RegisterContact
-} from '../controllers/authController';
+import { Router } from "express";
+import { LoginContact, RegisterContact } from "../controllers/authController";
 
 const router = Router();
 
-router.route('/auth/login').post(LoginContact);
-router.route('/auth/register').post(RegisterContact);
+router.route("/auth/login").post(LoginContact);
+router.route("/auth/register").post(RegisterContact);
 
 export default router;
